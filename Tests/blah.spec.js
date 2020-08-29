40ee1f452725f72117367746dea5a0540ee1a50b
Albert Woloszyn <awoloszyn@gmail.com>
2020-08-29 12:02:26 +0100

    Adding the spec file with URL

---
+++ Tests/blah.spec.js
@@ -0,0 +1,58 @@
+describe('It should be possible to access customer\'s area', function () {
+    beforeEach(function () {
+        browser.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
+    });
+    it('should have all elements in the header section', function () {
+        
+    });
+
+    it('xxx', function () {
+        
+    });
+
+    it('xxx', function () {
+        
+    });
+
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    it('xxx', function () {
+        
+    });
+    
+    
+});
